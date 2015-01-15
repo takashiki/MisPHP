800e0842919efc076ff009ef6dcb90572046214e
takashiki <857995137@qq.com>
2015-01-15 21:16:01 +0800

    v0.0.6

--- app/controller/Home.php
+++ app/controller/Home.php
@@ -1,11 +1,14 @@
 <?php
 use mis\Controller;
+use mis\View;
 
 class Home extends Controller
 {
   public function index() {
-    echo 'welcome seikai';
+    //echo 'welcome seikai';
     $this->m = new UserModel('user');
-    print_r($this->m->all());
+    //print_r($this->m->all());
+    
+    View::make('home/index', array('title' => 'mytitle', 'content' => 'hi you'));
   }
 }
